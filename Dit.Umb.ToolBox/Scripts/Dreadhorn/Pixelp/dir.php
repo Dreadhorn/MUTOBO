52e37c6c2d276ab11b3a07d537c939c4c6e54f32
Laurin Graf <Laurin.Graf@edu.gbssg.ch>
2021-07-18 23:01:39 +0200

    first test

---
+++ Dit.Umb.ToolBox/Scripts/Dreadhorn/Pixelp/dir.php
@@ -0,0 +1,6 @@
+﻿var files = <?php $out = array();
+foreach (glob('css/Pixelp/Bilder/*.jpg') as $filename) {
+    $p = pathinfo($filename);
+    $out[] = $p['filename'];
+}
+echo json_encode($out); ?>;
