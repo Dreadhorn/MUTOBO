ebd5f16b3bc3039a43c8a1abbc930652d5939e37
Laurin Graf <Laurin.Graf@edu.gbssg.ch>
2022-02-03 19:44:07 +0100

    einhorn and finger curser

---
+++ Dit.Umb.ToolBox/Scripts/Dreadhorn/cursor.js
@@ -0,0 +1,45 @@
+﻿
+var curser;
+var skript;
+
+function createElement(){
+    var cur = document.createElement('div')
+    cur.id = 'curser'
+    document.body.prepend(cur)
+}
+
+function keyFinger() {
+    createElement();
+    skript = '/scripts/Dreadhorn/svg/finger.js';
+}
+
+function keyEinhorn() {
+    createElement();
+    skript = '/scripts/Dreadhorn/svg/einhorn.js';
+}
+
+setTimeout(
+    function () {
+        $.getScript(skript, function () {
+            curser = new cursor();
+
+        }).then(() => {
+
+            $('#curser').html(curser.point);
+            document.body.style.cursor = "none";
+
+            $(this).mousedown(function (e) {
+                $('#curser').html(curser.down);
+            })
+
+            $(this).mouseup(function (e) {
+                $('#curser').html(curser.point);
+            })
+
+            $(this).mousemove(function (e) {
+                $('#curser').css({ top: e.pageY - curser.posY(), left: e.pageX - curser.posX() });
+            });
+        })
+    }, 100);
+
+
--- Dit.Umb.ToolBox/Scripts/main.js
+++ Dit.Umb.ToolBox/Scripts/main.js
@@ -1,2 +1,24 @@
 
-                        
+var keydown = "";
+var curs = false;
+
+document.addEventListener('keydown', function (event) {
+    keydown += event.key.toLowerCase();
+    if (!curs) {
+        if (keydown.includes("finger")) {
+            curs = true;
+            keydown = "";
+            $.getScript('/scripts/Dreadhorn/cursor.js', function () {
+                keyFinger();
+            });
+        }
+        else if (keydown.includes("einhorn")) {
+            curs = true;
+            keydown = "";
+            $.getScript('/scripts/Dreadhorn/cursor.js', function () {
+                keyEinhorn();
+            });
+        }
+    }
+});
+
